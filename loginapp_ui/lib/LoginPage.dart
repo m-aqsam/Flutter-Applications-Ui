a51cf69f8e21ac678f352c000d69f32acff9926d
m_aqsam <maqsam1155@gmail.com>
2024-03-14 01:00:02 +0500

    Login page add

---
+++ loginapp_ui/lib/LoginPage.dart
@@ -0,0 +1,141 @@
+// ignore_for_file: camel_case_types, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class LoginPage_Ui extends StatefulWidget {
+  const LoginPage_Ui({super.key});
+
+  @override
+  State<LoginPage_Ui> createState() => _LoginPage_UiState();
+}
+
+class _LoginPage_UiState extends State<LoginPage_Ui> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Icon
+            Icon(
+              Icons.android,
+              size: 180,
+            ),
+            // Hello Text
+
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Text(
+                  "Hello Again!",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 8),
+
+            // sub Text
+
+            Text(
+              "Welcome Back You have been Missed",
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+
+            SizedBox(height: 30),
+
+            // Email TextField
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 12),
+
+            // Password TextField
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 12),
+
+            // Sign In Button
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Container(
+                height: 50,
+                //width: 500,
+
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple[400],
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Center(
+                  child: Text(
+                    "Sign In",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 8),
+
+            // Not a member ? Register Now
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    "Not a Member? ",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Text(
+                    "Register Now",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
