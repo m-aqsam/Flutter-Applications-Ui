38c39e81ab05e082671b15a8096a587f20c5a72f
m_aqsam <maqsam1155@gmail.com>
2024-03-17 23:03:49 +0500

    Update HomeScreen

--- medical_app_ui/lib/screens/Home_Screen.dart
+++ medical_app_ui/lib/screens/Home_Screen.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: camel_case_types, prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:medical_app_ui/components/Doctor_Card.dart';
+import 'package:medical_app_ui/components/Medical_List.dart';
 import 'package:medical_app_ui/components/MyButton.dart';
 import 'package:medical_app_ui/components/MyTextField.dart';
 
@@ -54,11 +56,11 @@ class _Home_ScreenState extends State<Home_Screen> {
                     ),
 
                     Container(
-                      height: 60,
-                      width: 60,
+                      height: 50,
+                      width: 50,
                       decoration: BoxDecoration(
-                        color: Colors.deepPurple[200],
-                        borderRadius: BorderRadius.circular(60),
+                        color: Colors.deepPurple[100],
+                        borderRadius: BorderRadius.circular(12),
                       ),
                       child: Icon(
                         Icons.person,
@@ -79,20 +81,29 @@ class _Home_ScreenState extends State<Home_Screen> {
                   height: 170,
                   decoration: BoxDecoration(
                     color: Colors.pink[200],
-                    borderRadius: BorderRadius.circular(20),
+                    borderRadius: BorderRadius.circular(12),
                   ),
                   child: Padding(
-                    padding: const EdgeInsets.only(left: 20, right: 25),
+                    padding: const EdgeInsets.only(left: 25, right: 25),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         // Image
 
-                        Container(
-                          height: 130,
-                          width: 110,
-                          color: Colors.deepPurple[300],
+                        Image.asset(
+                          'lib/images/profile1.png',
+                          height: 135,
                         ),
+
+                        // Container(
+                        //   height: 120,
+                        //   width: 110,
+                        //   // color: Colors.deepPurple[300],
+
+                        //   child: Image.asset(
+                        //     'lib/images/profile2.png',
+                        //   ),
+                        // ),
 
                         // Text
                         Padding(
@@ -118,10 +129,10 @@ class _Home_ScreenState extends State<Home_Screen> {
 
                               Container(
                                 height: 40,
-                                width: 150,
+                                width: 190,
                                 decoration: BoxDecoration(
                                   color: Colors.deepPurple[300],
-                                  borderRadius: BorderRadius.circular(12),
+                                  borderRadius: BorderRadius.circular(10),
                                 ),
                                 child: Center(
                                   child: Text(
@@ -147,13 +158,150 @@ class _Home_ScreenState extends State<Home_Screen> {
               //Search Bar
 
               MyTextField(
-                  controller: controller,
-                  hintText: 'How can we help you?',
-                  obscureText: false),
+                controller: controller,
+                hintText: 'How can we help you?',
+                obscureText: false,
+              ),
+
+              SizedBox(height: 25),
 
               //Category ListView
 
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Container(
+                  height: 80,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      // Dentist
+                      Medical_List(
+                        image: 'lib/images/tooth.png',
+                        listName: 'Dentist',
+                      ),
+                      SizedBox(width: 15),
+
+                      // Doctor
+                      Medical_List(
+                        image: 'lib/images/stethoscope.png',
+                        listName: 'Doctor',
+                      ),
+                      SizedBox(width: 15),
+
+                      // Surgeon
+                      Medical_List(
+                        image: 'lib/images/knife.png',
+                        listName: 'Surgeon',
+                      ),
+                      SizedBox(width: 15),
+
+                      // Dietitian
+                      Medical_List(
+                        image: 'lib/images/laptop.png',
+                        listName: 'Dietitian',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 30),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Doctors List',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      'See all',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 25),
+
               // Doctors List
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Container(
+                  height: 200,
+                  //color: Colors.deepPurple[100],
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      // Card 1
+                      Doctor_Card(
+                        image: 'lib/images/doctor4.jpg',
+                        rating: 4.8,
+                        doctorName: 'Dr.Arleney McCoy',
+                        speciality: 'Therapist',
+                        years: 5,
+                      ),
+                      SizedBox(width: 20),
+
+                      // Card 2
+                      Doctor_Card(
+                        image: 'lib/images/doctor3.jpg',
+                        rating: 4.6,
+                        doctorName: 'Dr.Elizabeth Well',
+                        speciality: 'Therapist',
+                        years: 7,
+                      ),
+                      SizedBox(width: 20),
+
+                      // Card 3
+                      Doctor_Card(
+                        image: 'lib/images/doctor2.jpg',
+                        rating: 5.0,
+                        doctorName: 'Dr.Albert Flores',
+                        speciality: 'Therapist',
+                        years: 4,
+                      ),
+                      SizedBox(width: 20),
+
+                      // Card 4
+                      Doctor_Card(
+                        image: 'lib/images/doctor1.jpg',
+                        rating: 4.7,
+                        doctorName: 'Dr.Helen Brooke',
+                        speciality: 'Therapist',
+                        years: 3,
+                      ),
+                      SizedBox(width: 20),
+                    ],
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 28),
+              // Bottom Nav Bar
+
+              BottomNavigationBar(
+                type: BottomNavigationBarType.fixed,
+                items: [
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.home), label: 'Home'),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.message), label: 'Inbox'),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.notifications), label: 'Notification'),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.home), label: 'Home'),
+                ],
+              ),
             ],
           ),
         ),
