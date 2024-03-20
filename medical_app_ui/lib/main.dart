7cbc89f18681567939f6a5e36a8c7b27f24adeed
m_aqsam <maqsam1155@gmail.com>
2024-03-20 23:25:45 +0500

    Update

--- medical_app_ui/lib/main.dart
+++ medical_app_ui/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home_Screen(),
+      home: Splash_Screen1(),
     );
   }
 }
