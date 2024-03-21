0dfc312249c332fbda0cc924593cedb0440d0164
m_aqsam <maqsam1155@gmail.com>
2024-03-21 23:58:56 +0500

    User Pages

---
+++ tiktok_ui/lib/_NavScreens/User_InboxPage.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: camel_case_types, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class User_InboxPage extends StatelessWidget {
+  const User_InboxPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('InboxPage'),
+      ),
+    );
+  }
+}
