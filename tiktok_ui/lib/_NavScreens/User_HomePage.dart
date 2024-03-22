4f7402e411ac5b324fef3fb2163e9266675f25be
m_aqsam <maqsam1155@gmail.com>
2024-03-22 23:52:00 +0500

    Update

--- tiktok_ui/lib/_NavScreens/User_HomePage.dart
+++ tiktok_ui/lib/_NavScreens/User_HomePage.dart
@@ -1,15 +1,30 @@
-// ignore_for_file: camel_case_types, prefer_const_constructors
+// ignore_for_file: camel_case_types, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:tiktok_ui/HomePage_Components/Post2.dart';
+import 'package:tiktok_ui/HomePage_Components/Posts1.dart';
+import 'package:tiktok_ui/HomePage_Components/post3.dart';
 
 class User_HomePage extends StatelessWidget {
-  const User_HomePage({super.key});
+  User_HomePage({super.key});
+
+  final _controller = PageController(initialPage: 0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('HomePage'),
+      body: PageView(
+        controller: _controller,
+        scrollDirection: Axis.vertical,
+        children: [
+          // User Post Video
+
+          Post1(),
+          Post2(),
+          Post3(),
+
+          // Post Template (Post Info)
+        ],
       ),
     );
   }
