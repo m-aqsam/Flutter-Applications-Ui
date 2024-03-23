d35671736ce8942bc7da86c3d17aa2cd853863d1
m_aqsam <maqsam1155@gmail.com>
2024-03-23 05:52:41 +0500

    Update

--- tiktok_ui/lib/_NavScreens/User_HomePage.dart
+++ tiktok_ui/lib/_NavScreens/User_HomePage.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: camel_case_types, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:tiktok_ui/HomePage_Components/Post2.dart';
-import 'package:tiktok_ui/HomePage_Components/Posts1.dart';
-import 'package:tiktok_ui/HomePage_Components/post3.dart';
+import 'package:tiktok_ui/HomePage_Components/Posts/Post2.dart';
+import 'package:tiktok_ui/HomePage_Components/Posts/Post5.dart';
+import 'package:tiktok_ui/HomePage_Components/Posts/Posts1.dart';
+import 'package:tiktok_ui/HomePage_Components/Posts/post3.dart';
+import 'package:tiktok_ui/HomePage_Components/Posts/post4.dart';
 
 class User_HomePage extends StatelessWidget {
   User_HomePage({super.key});
@@ -22,6 +24,8 @@ class User_HomePage extends StatelessWidget {
           Post1(),
           Post2(),
           Post3(),
+          Post4(),
+          Post5(),
 
           // Post Template (Post Info)
         ],
