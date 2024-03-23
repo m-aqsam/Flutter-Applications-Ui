531cab3894b852d0e5e50a4b342473d0e3d17615
m_aqsam <maqsam1155@gmail.com>
2024-03-23 05:53:47 +0500

    Update

---
+++ tiktok_ui/lib/HomePage_Components/Posts/Posts1.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:tiktok_ui/HomePage_Components/PostButtons.dart';
+import 'package:tiktok_ui/HomePage_Components/Posts/Post_Template.dart';
+
+class Post1 extends StatelessWidget {
+  const Post1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PostTemplate(
+      username: "M Aqsam",
+      caption: "No Caption ❤️😩.",
+      hashtags: "#fyp #flutter #for_you #new",
+      likes: "49.4K",
+      comments: "3226",
+      favorite: "5002",
+      shares: "3273",
+      userpost: Container(
+        color: Colors.black,
+      ),
+    );
+  }
+}
