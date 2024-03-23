cf8f59e9c9ecccdba2a909af65bc6728757ab9e4
m_aqsam <maqsam1155@gmail.com>
2024-03-23 05:53:39 +0500

    Update

---
+++ tiktok_ui/lib/HomePage_Components/Posts/Post5.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_ui/HomePage_Components/Posts/Post_Template.dart';
+
+class Post5 extends StatelessWidget {
+  const Post5({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PostTemplate(
+      username: "Maryam Awan",
+      caption: "Teri Jhuki Nazr 🌚🕊️❤️.",
+      hashtags: "#viral #fyp #foryou #love",
+      likes: "29.4K",
+      comments: "164",
+      favorite: "3372",
+      shares: "590",
+      userpost: Container(
+        color: Colors.teal[700],
+      ),
+    );
+  }
+}
