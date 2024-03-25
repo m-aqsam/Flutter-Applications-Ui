d53179c3c5577ed131ee38c5a38d59236622ba8f
m_aqsam <maqsam1155@gmail.com>
2024-03-25 23:43:05 +0500

    Update 1

---
+++ wallet_app_ui/lib/main.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:wallet_app_ui/pages/Home_Page.dart';
+import 'package:wallet_app_ui/pages/MyListView.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home_Page(),
+    );
+  }
+}
