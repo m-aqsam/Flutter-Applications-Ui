7b57069a213d12c029724dee172bd887c6cbbca3
m_aqsam <maqsam1155@gmail.com>
2024-03-27 23:48:19 +0500

    new page add

---
+++ wallet_app_ui/lib/pages/New_Bill_Page.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class New_Bill_Page extends StatelessWidget {
+  const New_Bill_Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+          child: Column(
+        children: [
+          // Select Bill Type
+
+          // Bills Categories Type -> GridView
+        ],
+      )),
+    );
+  }
+}
