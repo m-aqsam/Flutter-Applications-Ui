1841234c113ec719d937a12aa0d94d5f7adf95e4
m_aqsam <maqsam1155@gmail.com>
2024-04-02 23:58:30 +0500

    shop page

---
+++ nike_ecommerce_app/lib/Pages/Shop_Page.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Shop_Page extends StatelessWidget {
+  const Shop_Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Shop Page"),
+    );
+  }
+}
