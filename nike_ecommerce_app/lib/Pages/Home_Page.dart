e575ffdad4c5c3120a9728f06f938c2ba3258229
m_aqsam <maqsam1155@gmail.com>
2024-04-02 23:58:16 +0500

    main app page

---
+++ nike_ecommerce_app/lib/Pages/Home_Page.dart
@@ -0,0 +1,87 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, prefer_final_fields
+
+import 'package:flutter/material.dart';
+import 'package:nike_ecommerce_app/Pages/Add_To_Cart_Page.dart';
+import 'package:nike_ecommerce_app/Pages/Shop_Page.dart';
+import 'package:nike_ecommerce_app/components/MyNavigation_Bar.dart';
+
+class Home_Page extends StatefulWidget {
+  const Home_Page({super.key});
+
+  @override
+  State<Home_Page> createState() => _Home_PageState();
+}
+
+class _Home_PageState extends State<Home_Page> {
+  int _selectedIndex = 0;
+
+  // navigateBottomBar Method
+
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  // List of Bottom Nav Bar Tabs
+
+  List<Widget> _pages = [
+    Shop_Page(),
+    Add_To_Cart_Page(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+
+      // ***************** App Bar ********************
+
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: Builder(
+          builder: (context) => IconButton(
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+            icon: Icon(Icons.menu),
+          ),
+        ),
+
+        //  tittle
+
+        title: Text("Nike Store"),
+      ),
+
+      // *****************  Drawer *******************
+
+      drawer: Drawer(
+        backgroundColor: Colors.grey[900],
+        child: Column(
+          children: [
+            // logo
+
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 60),
+              child: Image.asset(
+                "lib/images/nikelogo1.png",
+                height: 200,
+                color: Colors.white,
+              ),
+            )
+
+            // Buttons
+          ],
+        ),
+      ),
+
+      // **************** Bottom Nav Bar **************
+
+      bottomNavigationBar: MyNavigation_Bar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+
+      body: _pages[_selectedIndex],
+    );
+  }
+}
