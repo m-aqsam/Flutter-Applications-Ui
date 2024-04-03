0d612ec7a77a337ea9237c932b04e0d237d20e59
m_aqsam <maqsam1155@gmail.com>
2024-04-03 23:39:30 +0500

    update

---
+++ nike_ecommerce_app/lib/Pages/Profile_Page.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Profile_Page extends StatelessWidget {
+  const Profile_Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Profile Page"),
+    );
+  }
+}
