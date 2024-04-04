e28421cc9bcb1205ffa1707a018c76af6cf78a30
m_aqsam <maqsam1155@gmail.com>
2024-04-04 23:23:16 +0500

    update

--- nike_ecommerce_app/lib/Pages/Profile_Page.dart
+++ nike_ecommerce_app/lib/Pages/Profile_Page.dart
@@ -7,8 +7,8 @@ class Profile_Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Profile Page"),
+    return Column(
+      children: [],
     );
   }
 }
