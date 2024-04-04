24a0fef5d7e70fda554766419b5523fd57fb3a1f
m_aqsam <maqsam1155@gmail.com>
2024-04-04 23:17:42 +0500

    new update

--- nike_ecommerce_app/lib/main.dart
+++ nike_ecommerce_app/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false, 
       home: Intro_Page(),
     );
   }
