6fbeba52e47ea7c6084a0c19e0a58ce93a364328
m_aqsam <maqsam1155@gmail.com>
2024-04-10 04:32:34 +0500

    update

--- nike_ecommerce_app/lib/Pages/Add_To_Cart_Page.dart
+++ nike_ecommerce_app/lib/Pages/Add_To_Cart_Page.dart
@@ -1,9 +1,15 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:nike_ecommerce_app/components/MyCartList.dart';
 
 class Add_To_Cart_Page extends StatelessWidget {
   const Add_To_Cart_Page({super.key});
+
+  //  ********************** Issue ***********************
+
+  //  Want to access the List(_shoeList) in ListView File
+  //  to this file and and add items in it
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +17,33 @@ class Add_To_Cart_Page extends StatelessWidget {
         backgroundColor: Colors.grey[300],
         body: Column(
           children: [
-            //
+            //  name
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "My Cart ()",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 20),
+
+            // Cart Items
+
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    // color: Colors.deepPurple[200],
+                    ),
+                child: MyCartList(),
+              ),
+            ),
           ],
         ));
   }
