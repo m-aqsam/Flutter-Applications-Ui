7392f86f69ea9345a620649e1fbc2e0cfd25d1d6
m_aqsam <maqsam1155@gmail.com>
2024-04-09 23:42:09 +0500

    update

--- nike_ecommerce_app/lib/Pages/Profile_Page.dart
+++ nike_ecommerce_app/lib/Pages/Profile_Page.dart
@@ -1,15 +1,73 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
 
 import 'package:flutter/material.dart';
+import 'package:nike_ecommerce_app/components/ProfileOption.dart';
+import 'package:image_picker/image_picker.dart';
 
-class Profile_Page extends StatelessWidget {
+class Profile_Page extends StatefulWidget {
   const Profile_Page({super.key});
+
+  @override
+  State<Profile_Page> createState() => _Profile_PageState();
+}
+
+class _Profile_PageState extends State<Profile_Page> {
+  String? imagePath;
+
+  // Change Avatar Method
+  void changeAvatar() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.grey[300],
+          title: Text("Change Avatar"),
+          content: Text("Select an image from the gallery"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Cancel"),
+            ),
+
+            // Select Bu
+            TextButton(
+              onPressed: () async {
+                // Pick an image from the gallery
+
+                // Here Start of Method
+
+                //
+                // final imagePicker = ImagePicker();
+                // final XFile? pickedFile =
+                //     await imagePicker.pickImage(source: ImageSource.gallery);
+
+                // if (pickedFile != null) {
+                //   // Update the profile image
+                //   setState(() {
+                //     // Assuming you have a variable to store the image path
+                //     imagePath = pickedFile.path;
+                //   }
+
+                //   );
+                // }
+
+                Navigator.of(context).pop();
+              },
+              child: Text("Select"),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        // Page Tittle
+        // Page Name
 
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -17,35 +75,88 @@ class Profile_Page extends StatelessWidget {
             Text(
               "My Profile",
               style: TextStyle(
-                fontSize: 20,
+                fontSize: 25,
                 fontWeight: FontWeight.bold,
               ),
-            )
+            ),
           ],
         ),
-
-        SizedBox(height: 25),
-
+        SizedBox(height: 30),
         // Profile Photo
 
         CircleAvatar(
-          backgroundImage: AssetImage("lib/images/man.jpg"),
-          // backgroundColor: Colors.black,
+          backgroundImage: AssetImage("lib/images/profileimage1.jpg"),
           radius: 75,
         ),
 
-        SizedBox(height: 20),
+        SizedBox(height: 18),
 
         // User Name
 
         Text(
           "Amelia Jacob",
           style: TextStyle(
-            fontSize: 20,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
           ),
-        )
+        ),
+
+        SizedBox(height: 30),
 
         // Profile Options
+
+        ProfileOption(
+          optionName: "Change Avatar",
+          onTap: changeAvatar,
+        ),
+        ProfileOption(
+          optionName: "Settings",
+          onTap: () {
+            // Code
+          },
+        ),
+        ProfileOption(
+          optionName: "My Orders",
+          onTap: () {
+            // Code
+          },
+        ),
+        ProfileOption(
+          optionName: "Preferences",
+          onTap: () {
+            // Code
+          },
+        ),
+        ProfileOption(
+          optionName: "Help \& Support",
+          onTap: () {
+            // Code
+          },
+        ),
+
+        SizedBox(height: 15),
+
+        // Logout
+
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 50),
+          child: Container(
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Center(
+              child: Text(
+                "Logout",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ),
+        ),
 
         //
       ],
