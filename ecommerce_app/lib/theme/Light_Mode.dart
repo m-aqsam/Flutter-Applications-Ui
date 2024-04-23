a6d62dc10db83cde6c5917eb817243336d50f76c
m_aqsam <maqsam1155@gmail.com>
2024-04-23 23:53:32 +0500

    theme update

---
+++ ecommerce_app/lib/theme/Light_Mode.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: use_full_hex_values_for_flutter_colors, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+    colorScheme: ColorScheme.light(
+  background: Colors.grey.shade300,
+  primary: Colors.white,
+  secondary: Colors.grey.shade400,
+  inversePrimary: Colors.grey.shade900,
+));
