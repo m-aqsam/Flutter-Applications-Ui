e112c69aba50f61e87ed873eb06af94f6ee66bcb
m_aqsam <maqsam1155@gmail.com>
2024-04-24 20:37:05 +0500

    update

---
+++ ecommerce_app/lib/components/Search_Bar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Search_Bar extends StatelessWidget {
+  const Search_Bar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Container(
+        padding: EdgeInsets.only(left: 18, right: 10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: TextField(
+          cursorColor: Colors.grey[700],
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: "Search",
+            suffixIcon: Icon(
+              Icons.search,
+              color: Colors.grey[00],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
