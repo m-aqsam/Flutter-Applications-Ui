72329c362521ffe054713bf23ba330a02f4e1d4c
m_aqsam <maqsam1155@gmail.com>
2024-04-24 23:54:15 +0500

    updated

--- ecommerce_app/lib/pages/Shop_Page.dart
+++ ecommerce_app/lib/pages/Shop_Page.dart
@@ -1,17 +1,16 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:ecommerce_app/components/My_Product_Tile.dart';
 import 'package:ecommerce_app/components/Search_Bar.dart';
+import 'package:ecommerce_app/models/Product.dart';
+import 'package:ecommerce_app/models/Shop.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class Shop_Page extends StatefulWidget {
-  const Shop_Page({super.key});
+class Shop_Page extends StatelessWidget {
+  Shop_Page({super.key});
 
-  @override
-  State<Shop_Page> createState() => _Shop_PageState();
-}
-
-class _Shop_PageState extends State<Shop_Page> {
   final List<Widget> carouselImages = [
     // Carousl Images
     Image.asset("lib/images/carouselimage1.png"),
@@ -29,7 +28,6 @@ class _Shop_PageState extends State<Shop_Page> {
   ];
 
   // Map
-
   final Map<String, String> imagePaths = {
     "image1": "lib/images/carouselimage1.png",
     "image2": "lib/images/carouselimage2.png",
@@ -49,6 +47,7 @@ class _Shop_PageState extends State<Shop_Page> {
   //
   @override
   Widget build(BuildContext context) {
+    final products = context.watch<Shop>().shop;
     return Scaffold(
       // *****************  Scaffold *******************
 
@@ -59,13 +58,13 @@ class _Shop_PageState extends State<Shop_Page> {
 
           CarouselSlider(
             options: CarouselOptions(
-              height: 180,
+              height: 185,
               aspectRatio: 16 / 9,
-              viewportFraction: 1,
+              viewportFraction: 1.0,
               initialPage: 0,
               enableInfiniteScroll: true,
               reverse: false,
-              // autoPlay: true,
+              autoPlay: true,
               autoPlayInterval: Duration(seconds: 4),
               autoPlayAnimationDuration: Duration(milliseconds: 800),
               autoPlayCurve: Curves.fastOutSlowIn,
@@ -88,11 +87,37 @@ class _Shop_PageState extends State<Shop_Page> {
 
           SizedBox(height: 10),
 
-          // Search Bar
+          // **************** Search Bar
 
           Search_Bar(),
 
-          //
+          // ************** Product List
+
+          Expanded(
+            child: GridView.builder(
+              scrollDirection: Axis.vertical,
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount:
+                    MediaQuery.of(context).orientation == Orientation.portrait
+                        ? 2
+                        : 1, // Set crossAxisCount based on orientation
+                //crossAxisSpacing: 10,
+                //mainAxisSpacing: 10,
+                childAspectRatio: 0.8,
+              ),
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                // get each individual product from shop
+                final product = products[index];
+
+                // return as a product tile in ui
+                return My_Product_Tile(product: product);
+              },
+            ),
+          ),
+
+//
         ],
       ),
     );
