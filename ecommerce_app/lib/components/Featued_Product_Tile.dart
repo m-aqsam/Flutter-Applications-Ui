6538d9fe4f98259878b1ccd9b767796059dd6774
m_aqsam <maqsam1155@gmail.com>
2024-04-25 23:41:11 +0500

    new update

---
+++ ecommerce_app/lib/components/Featued_Product_Tile.dart
@@ -0,0 +1,74 @@
+import 'package:ecommerce_app/models/Product.dart';
+import 'package:flutter/material.dart';
+
+class Featured_Product_Tile extends StatelessWidget {
+  final Product product;
+  const Featured_Product_Tile({
+    super.key,
+    required this.product,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 100,
+        width: 110,
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.primary,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          children: [
+            // Product Image
+
+            Container(
+              padding: EdgeInsets.all(28),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.secondary,
+                  borderRadius: BorderRadius.circular(12)),
+              child: Icon(Icons.favorite),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(left: 7, right: 7, top: 2),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Product Name
+
+                      Text(
+                        product.name,
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                      ),
+
+                      // Product Price
+
+                      Text(product.price.toStringAsFixed(2)),
+
+                      // Add to Cart Button
+                    ],
+                  ),
+
+                  // Button
+
+                  Container(
+                    // color: Colors.amber,
+                    child: Icon(Icons.favorite_border_outlined),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
