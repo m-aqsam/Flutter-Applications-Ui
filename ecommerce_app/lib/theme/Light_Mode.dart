e136c0b34c702e5c838e9b3fd4253f78ade748f4
m_aqsam <maqsam1155@gmail.com>
2024-04-25 23:41:45 +0500

    new update

--- ecommerce_app/lib/theme/Light_Mode.dart
+++ ecommerce_app/lib/theme/Light_Mode.dart
@@ -6,6 +6,7 @@ ThemeData lightMode = ThemeData(
     colorScheme: ColorScheme.light(
   background: Colors.grey.shade300,
   primary: Colors.white,
-  secondary: Colors.grey.shade200,
+  secondary: Colors.grey.shade100,
   inversePrimary: Colors.grey.shade900,
+  onSecondary: Colors.grey.shade700,
 ));
