ba17921f53f701c10a22fcab70885f9747dcc41e
m_aqsam <maqsam1155@gmail.com>
2024-04-28 03:44:24 +0500

    updated

---
+++ ecommerce_app/lib/models/Feature_List.dart
@@ -0,0 +1,69 @@
+import 'package:ecommerce_app/models/Featured_Product.dart';
+import 'package:flutter/material.dart';
+
+class FeaturedList extends ChangeNotifier {
+  // Featured Products List
+
+  final List<FeaturedProduct> _items = [
+    // Product 1
+
+    FeaturedProduct(
+      productName: "Headset",
+      price: 99.9,
+      imagePath: "lib/images/featuredproducts/product1.jpg",
+    ),
+    // Product 2
+
+    FeaturedProduct(
+      productName: "Laptop",
+      price: 99.9,
+      imagePath: "lib/images/featuredproducts/product2.jpg",
+    ),
+    // Product 3
+
+    FeaturedProduct(
+      productName: "Shoes",
+      price: 99.9,
+      imagePath: "lib/images/featuredproducts/product3.jpg",
+    ),
+    // Product 4
+
+    FeaturedProduct(
+      productName: "Rings",
+      price: 99.9,
+      imagePath: "lib/images/featuredproducts/product4.jpg",
+    ),
+    // Product 5
+
+    FeaturedProduct(
+      productName: "Cosmetic",
+      price: 99.9,
+      imagePath: "lib/images/featuredproducts/product5.jpg",
+    ),
+  ];
+
+  // User Liked Items List
+
+  final List<FeaturedProduct> _likedItems = [];
+
+  // Get Featured Items
+  List<FeaturedProduct> get items => _items;
+
+  // Get Liked Items List
+
+  List<FeaturedProduct> get likedItems => _likedItems;
+
+// Add to Liked Items List
+
+  void addedToLiked(FeaturedProduct item) {
+    _likedItems.add(item);
+    notifyListeners();
+  }
+
+// Remove From Liked Items
+
+  void removeFromLikedItems(FeaturedProduct item) {
+    _likedItems.remove(item);
+    notifyListeners();
+  }
+}
