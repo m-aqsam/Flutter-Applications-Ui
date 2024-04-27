4a6709a55cd2d1d67586ba73b52db6e847fee9b2
m_aqsam <maqsam1155@gmail.com>
2024-04-28 03:44:49 +0500

    new products

---
+++ ecommerce_app/lib/models/Featured_Product.dart
@@ -0,0 +1,11 @@
+class FeaturedProduct {
+  final String productName;
+  final double price;
+  final String imagePath;
+
+  FeaturedProduct({
+    required this.productName,
+    required this.price,
+    required this.imagePath,
+  });
+}
