3e2d9d2d1a6a8beb952df42c1dfe8141826562d1
m_aqsam <maqsam1155@gmail.com>
2024-05-06 23:50:27 +0500

    updated

--- ecommerce_app/lib/components/Carousel_Slider.dart
+++ ecommerce_app/lib/components/Carousel_Slider.dart
@@ -1,13 +1,73 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class Carusel_Slider extends StatelessWidget {
-  const Carusel_Slider({super.key});
+  Carusel_Slider({super.key});
+
+  final List<Widget> carouselImages = [
+    // Carousl Images
+    Image.asset("lib/images/carouselimage1.png"),
+    Image.asset("lib/images/carouselimage2.png"),
+    Image.asset("lib/images/carouselimage3.png"),
+    Image.asset("lib/images/carouselimage4.png"),
+    Image.asset("lib/images/carouselimage5.png"),
+    Image.asset("lib/images/carouselimage6.png"),
+    Image.asset("lib/images/carouselimage7.png"),
+    Image.asset("lib/images/carouselimage8.png"),
+    Image.asset("lib/images/carouselimage9.png"),
+    Image.asset("lib/images/carouselimage10.png"),
+    Image.asset("lib/images/carouselimage11.png"),
+    Image.asset("lib/images/carouselimage12.png"),
+  ];
+
+  // Map
+  final Map<String, String> imagePaths = {
+    "image1": "lib/images/carouselimage1.png",
+    "image2": "lib/images/carouselimage2.png",
+    "image3": "lib/images/carouselimage3.png",
+    "image4": "lib/images/carouselimage4.png",
+    "image5": "lib/images/carouselimage5.png",
+    "image6": "lib/images/carouselimage6.png",
+    "image7": "lib/images/carouselimage7.png",
+    "image8": "lib/images/carouselimage8.png",
+    "image9": "lib/images/carouselimage9.png",
+    "image10": "lib/images/carouselimage10.png",
+    "image11": "lib/images/carouselimage11.png",
+    "image12": "lib/images/carouselimage12.png",
+    // Add more image paths as needed
+  };
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return // Carlousel Slider
 
-        // Paste the carousel Code from main shop file ()
+        CarouselSlider(
+      options: CarouselOptions(
+        height: 185,
+        aspectRatio: 16 / 9,
+        viewportFraction: 1.0,
+        initialPage: 0,
+        enableInfiniteScroll: true,
+        reverse: false,
+        autoPlay: true,
+        autoPlayInterval: Duration(seconds: 4),
+        autoPlayAnimationDuration: Duration(milliseconds: 800),
+        autoPlayCurve: Curves.fastOutSlowIn,
+        //enlargeCenterPage: true,
+        enlargeFactor: 0.5,
+        scrollDirection: Axis.horizontal,
+      ),
+      // items: carouselImages,
+
+      items: imagePaths.keys.map((key) {
+        return SizedBox(
+          width: double.maxFinite,
+          child: Image.asset(
+            imagePaths[key]!,
+            fit: BoxFit.cover,
+          ),
         );
+      }).toList(),
+    );
   }
 }
