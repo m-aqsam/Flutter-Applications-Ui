1e9bbb7dc8388b8c743ea991691a9bfd9910ebff
m_aqsam <maqsam1155@gmail.com>
2024-05-08 09:27:13 +0500

    profile component

---
+++ ecommerce_app/lib/components/MyProfileList.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class MyProfileList extends StatelessWidget {
+  final String imagePath;
+  final String text;
+  MyProfileList({
+    super.key,
+    required this.imagePath,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          // Icon
+
+          //lib\images\profileimages
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.grey[100],
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Image.asset(
+                imagePath,
+                height: 45,
+              ),
+            ),
+          ),
+
+          // Text
+
+          Text(
+            text,
+            style: TextStyle(
+              fontSize: 12,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
