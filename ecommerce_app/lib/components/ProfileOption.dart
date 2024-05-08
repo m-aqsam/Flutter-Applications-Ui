cdabed37a1794025abc2080a726fb481d5c0ff32
m_aqsam <maqsam1155@gmail.com>
2024-05-08 09:27:22 +0500

    component

---
+++ ecommerce_app/lib/components/ProfileOption.dart
@@ -0,0 +1,48 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class ProfileOption extends StatelessWidget {
+  final String optionName;
+  final void Function()? onTap;
+  ProfileOption({
+    super.key,
+    required this.optionName,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 12, right: 12, bottom: 10),
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          height: 50,
+          decoration: BoxDecoration(
+            color: Colors.grey[300],
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // Name
+
+              Text(
+                optionName,
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Icon(
+                Icons.navigate_next_rounded,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
