724f793430bc9e97def960e65151d1b7e5867f7a
m_aqsam <maqsam1155@gmail.com>
2024-09-22 23:14:55 +0500

    Update

--- wallet_app_ui/lib/main.dart
+++ wallet_app_ui/lib/main.dart
@@ -1,21 +1,14 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:wallet_app_ui/pages/Home_Page.dart';
+import 'package:wallet_app_ui/MyApp.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home_Page(),
-    );
-  }
-}
+  // * Built using Flutter with ❤️ by Aqsam.
+  // ! Give Your Feedback
+  // ? Got any question
+  // Todo: Let's Connect
+  
