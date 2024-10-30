f3104b1f5faaeea3708dd0cae9801613bd8c2c4a
Aqsam <maqsam1155@gmail.com>
2024-10-30 23:56:27 +0500

    update

--- tiktok_ui/lib/main.dart
+++ tiktok_ui/lib/main.dart
@@ -6,3 +6,8 @@ import 'package:tiktok_ui/app.dart';
 void main() {
   runApp(const MyApp());
 }
+
+// * Built using Flutter with ❤️ by Aqsam.
+// ! Give Your Feedback
+// ? Got any question
+// Todo: Let's Connect
