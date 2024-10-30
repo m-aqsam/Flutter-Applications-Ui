ac1f95d876a59106cbef279ff31d17f9f4f69619
Aqsam <maqsam1155@gmail.com>
2024-10-30 23:54:35 +0500

    new changes

--- tiktok_ui/lib/main.dart
+++ tiktok_ui/lib/main.dart
@@ -1,24 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:tiktok_ui/MainPage.dart';
+import 'package:tiktok_ui/app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          // brightness: Brightness.dark,
-          ),
-      home: MainPage(),
-    );
-  }
 }
