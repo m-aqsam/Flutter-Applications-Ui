25bbd94506814377694b507a6f7fc6d02aeface9
Aqsam <maqsam1155@gmail.com>
2024-11-12 23:49:07 +0500

    some new updates

--- ecommerce_app/lib/theme/Light_Mode.dart
+++ ecommerce_app/lib/theme/Light_Mode.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
     colorScheme: ColorScheme.light(
-  background: Colors.grey.shade300,
+  surface: Colors.grey.shade300,
   primary: Colors.white,
   secondary: Colors.grey.shade100,
   inversePrimary: Colors.grey.shade900,
