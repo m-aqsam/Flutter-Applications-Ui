fe513951794e366f775fa829c2bc7f95a0c54305
Aqsam <maqsam1155@gmail.com>
2024-11-12 23:49:36 +0500

    new update

--- tiktok_ui/lib/ProfilePage_Components/Tabs/04_Favourite_Video_Tab.dart
+++ tiktok_ui/lib/ProfilePage_Components/Tabs/04_Favourite_Video_Tab.dart
@@ -8,7 +8,7 @@ class Favourite_Video_Tab extends StatelessWidget {
     return GridView.builder(
       itemCount: 5,
       gridDelegate:
-          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
       itemBuilder: (context, index) {
         return Padding(
           padding: const EdgeInsets.all(1.5),
