2115e9ad004ac0ce17866a0bc6a128e30ac894bf
Aqsam <maqsam1155@gmail.com>
2024-11-15 00:41:22 +0500

    update

--- ecommerce_app/lib/models/Shop.dart
+++ ecommerce_app/lib/models/Shop.dart
@@ -1,6 +1,5 @@
 import 'package:ecommerce_app/models/Product.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class Shop extends ChangeNotifier {
   //   Product for Sale
